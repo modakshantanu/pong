8a4f3e721f22ce2840cfb44f2812b1aba035773b
modakshantanu <modakshantanu@gmail.com>
2019-07-01 13:36:39 +0800

    Updates

---
+++ precache-manifest.fb7613609a5f2ba4da12e33d530fea44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53c8f3e32cf68a63936fa148508591e6",
+    "url": "/pong/index.html"
+  },
+  {
+    "revision": "91097559c5de602a9d49",
+    "url": "/pong/static/css/main.a9b33847.chunk.css"
+  },
+  {
+    "revision": "fd03c1ca426d2c88ee14",
+    "url": "/pong/static/js/2.f30055dd.chunk.js"
+  },
+  {
+    "revision": "91097559c5de602a9d49",
+    "url": "/pong/static/js/main.edb958dc.chunk.js"
+  },
+  {
+    "revision": "a417f954ab3244e00dfb",
+    "url": "/pong/static/js/runtime~main.a500ca0d.js"
+  }
+]);
