9802fb80e8f549aa8f95255f4c3160c2b2d6ab80
modakshantanu <modakshantanu@gmail.com>
2020-02-17 15:21:14 +0800

    Updates

---
+++ precache-manifest.05f3646f60c5313cdfe279eb789db681.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e40d260b368c90a6e00cb1a3dd9014ec",
+    "url": "/pong/index.html"
+  },
+  {
+    "revision": "89238d80a51cb1eba11f",
+    "url": "/pong/static/css/main.c0e1b3ac.chunk.css"
+  },
+  {
+    "revision": "48f16522c31473b6c25a",
+    "url": "/pong/static/js/2.1fc87c06.chunk.js"
+  },
+  {
+    "revision": "89238d80a51cb1eba11f",
+    "url": "/pong/static/js/main.6e524abe.chunk.js"
+  },
+  {
+    "revision": "a417f954ab3244e00dfb",
+    "url": "/pong/static/js/runtime~main.a500ca0d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pong/precache-manifest.f89bbb871ff3ac502f2970f7cbefb4da.js"
+  "/pong/precache-manifest.05f3646f60c5313cdfe279eb789db681.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pong/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
